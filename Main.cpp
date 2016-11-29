5fea25e9fe59c19cd992fed0e6e27915f1c09819
Andrew Klem <amklem@gmail.com>
2016-11-29 12:44:30 -0500

    Add reshape function that maintains aspect ratio

--- Main.cpp
+++ Main.cpp
@@ -15,9 +15,11 @@ void myInit(void)
 	glColor3f(0.0f, 0.0f, 0.0f);		// drawing color: white
 	glLineWidth(2.0);					// a line is 2 pixels wide
 
-	glMatrixMode(GL_PROJECTION);		// set matrix mode
-	glLoadIdentity();					// lead identity matrix
+	glMatrixMode(GL_PROJECTION);							// set matrix mode
+	glLoadIdentity();										// lead identity matrix
+	glOrtho(-10.0f, 10.0f, -10.0f, 10.0f, -10.0f, 10.0f);	// orthographic mapping
 
+	// set up ability to track object depths
 	glClearDepth(1.0f);					// set clearing distance for depth buffer
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LEQUAL);
@@ -32,6 +34,25 @@ void display(void)
 	glutSwapBuffers();
 }
 
+// reshape callback
+void reshape(int w, int h) {
+
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	if (w <= h) {
+		glOrtho(-10.0f, 10.0f, -10.0f * (GLfloat)h / (GLfloat)w, 10.0f * (GLfloat)h / (GLfloat)w, -10.0f, 10.0f);
+	}
+	else {
+		glOrtho(-10.0f * (GLfloat)w / (GLfloat)h, 10.0f * (GLfloat)w / (GLfloat)h, -10.0f, 10.0f, -10.0f, 10.0f);
+	}
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+}
+
 int main(int argc, char** argv)
 {
 	// basic glut setup
@@ -43,7 +64,8 @@ int main(int argc, char** argv)
 	
 	// register callback functions
 	myInit();						// additional inits
-	glutDisplayFunc(display);		// redraw
+	glutDisplayFunc(display);		// redraw to window
+	glutReshapeFunc(reshape);		// reshape window
 
 	glutMainLoop();
 
